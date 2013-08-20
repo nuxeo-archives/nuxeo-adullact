1a9797769f598de8f7e61672c37c1c8b80a6589d
Benjamin JALON <bjalon@nuxeo.com>
2013-08-20 16:29:44 +0200

    NXP-12261: Add missing precedence information to override default StartupHelper

--- nuxeo-adullact-webdelib-parent/nuxeo-adullact-webdelib-core/src/main/java/org/nuxeo/adullact/webdelib/WebDelibStartupHelper.java
+++ nuxeo-adullact-webdelib-parent/nuxeo-adullact-webdelib-core/src/main/java/org/nuxeo/adullact/webdelib/WebDelibStartupHelper.java
@@ -1,10 +1,12 @@
 package org.nuxeo.adullact.webdelib;
 
 import static org.jboss.seam.ScopeType.SESSION;
+import static org.jboss.seam.annotations.Install.APPLICATION;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.nuxeo.ecm.core.api.ClientException;
@@ -12,12 +14,14 @@ import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.api.PathRef;
 import org.nuxeo.ecm.webapp.helpers.StartupHelper;
 
+
 /**
  * Overriding default {@see org.nuxeo.ecm.webapp.helpers.StartupHelper} to
  * redirect user into the default domain if available or dashboard after login in.
  */
 @Name("startupHelper")
 @Scope(SESSION)
+@Install(precedence = APPLICATION)
 public class WebDelibStartupHelper extends StartupHelper {
 
     private static final Log log = LogFactory.getLog(WebDelibStartupHelper.class);
