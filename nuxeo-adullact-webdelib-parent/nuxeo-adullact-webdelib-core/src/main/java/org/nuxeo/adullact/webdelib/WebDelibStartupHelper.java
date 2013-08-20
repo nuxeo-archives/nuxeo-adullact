ad74640ea1b4dafd1259f87b574c17d2678fa689
Benjamin JALON <bjalon@nuxeo.com>
2013-08-20 10:34:41 +0200

    NXP-12261: Override default startup helper to change default destination after connection

---
+++ nuxeo-adullact-webdelib-parent/nuxeo-adullact-webdelib-core/src/main/java/org/nuxeo/adullact/webdelib/WebDelibStartupHelper.java
@@ -0,0 +1,42 @@
+package org.nuxeo.adullact.webdelib;
+
+import static org.jboss.seam.ScopeType.SESSION;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.webapp.helpers.StartupHelper;
+
+/**
+ * Overriding default {@see org.nuxeo.ecm.webapp.helpers.StartupHelper} to
+ * redirect user into the default domain if available or dashboard after login in.
+ */
+@Name("startupHelper")
+@Scope(SESSION)
+public class WebDelibStartupHelper extends StartupHelper {
+
+    private static final Log log = LogFactory.getLog(WebDelibStartupHelper.class);
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    @Begin(id = "#{conversationIdGenerator.nextMainConversationId}", join = true)
+    public String initDomainAndFindStartupPage(String domainTitle, String viewId) {
+        super.initDomainAndFindStartupPage(domainTitle, viewId);
+
+        DocumentRef domainRef = new PathRef("/default-domain");
+        try {
+            if ( documentManager.exists(domainRef) ) {
+                return navigationContext.navigateToRef(domainRef);
+            }
+        } catch (ClientException e) {
+            log.error("Error during default Domain fetch, navigate into the Dashboard", e);
+        }
+        return dashboardNavigationHelper.navigateToDashboard();
+    }
+}
